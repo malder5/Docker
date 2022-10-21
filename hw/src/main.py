8fe904d27cb7497939beb6b6bfdfaef8089563f8
Roman Mulyarchik <123@mail.ru>
2022-10-21 18:01:24 +0300

    add new project

---
+++ hw/src/main.py
@@ -0,0 +1,14 @@
+#!/etc/bin/env python
+import time
+
+import requests
+
+def get_urls(file='data/file.txt'):
+    data = []
+    with open(file, 'r', encoding='utf8') as f:
+        for line in f.readlines():
+            r = requests.get(line.strip()+"/favicon.ico")
+            # print(line)
+
+if __name__ == '__main__':
+    get_urls()
